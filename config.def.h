3af2c58ecf27a503c9ac614292e9edf29ed6d910
Lingchu Xi <xilingchu@163.com>
2020-12-15 00:19:23 +0800

    Scroll patch

--- config.def.h
+++ config.def.h
@@ -215,6 +215,10 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i =  1} },
+	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i =  1} },
+	{ MODKEY|ShiftMask,     XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ MODKEY|ShiftMask,     XK_Page_Down,   kscrolldown,    {.i = -1} },
 	{ XK_ANY_MOD,           XK_F6,          swapcolors,     {.i =  0} },
 };
 
